7df8e1f38a99c01b31d23a748b7db730310209c9
Phan Binh <binh.phan.vic@gmail.com>
2023-06-04 21:34:50 +0700

    update path base service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { IntegrationCarService } from './features/car/services';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private _integrationCarService: IntegrationCarService,
   ) {}
 
   @Get()
   async getHello() {
-    await this._integrationCarService.findOne({});
     return this.appService.getHello();
   }
 }
