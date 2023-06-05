24d695f12e2b7955589687241d732a1c9ecac0d7
Binh Phan <binh.phan@vn-nest.tech>
2023-06-05 09:34:24 +0700

    init base service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IntegrationCarService } from './features/car/services';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
+    private integrationCarService: IntegrationCarService,
   ) {}
 
   @Get()
   async getHello() {
+    await this.integrationCarService.findOne({});
     return this.appService.getHello();
   }
 }
