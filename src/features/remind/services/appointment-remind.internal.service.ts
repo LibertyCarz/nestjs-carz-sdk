dfdf667d2d5d4c546d16b0acffc2e8859dbb58fd
Quan Luong <lhongquan.1998@gmail.com>
2023-08-04 19:07:03 +0700

    update remind payload

--- src/features/remind/services/appointment-remind.internal.service.ts
+++ src/features/remind/services/appointment-remind.internal.service.ts
@@ -3,7 +3,7 @@ import { ClientProxy } from '@nestjs/microservices';
 
 import { CMD, SERVICES } from '../../../constants';
 import { BaseSdkEventPayloadRequest } from '../../../shared/base.request';
-import { SdkUpdateRemindRedisPayload } from '../dto';
+import { SdkBookingRemind, SdkUpdateRemindRedisPayload } from '../dto';
 
 @Injectable()
 export class AppointmentRemindInternalService {
@@ -12,7 +12,7 @@ export class AppointmentRemindInternalService {
   ) {}
 
   public async updateRemindMerchantRedisCount(
-    payload: BaseSdkEventPayloadRequest<SdkUpdateRemindRedisPayload>,
+    payload: BaseSdkEventPayloadRequest<SdkBookingRemind>,
   ) {
     return this._carzNotification.emit(
       CMD.CARZ_APPOINTMENT_REMIND,
