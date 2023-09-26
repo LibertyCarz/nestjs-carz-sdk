ca385db80690b6ce7621a34306fb898a3ba812f2
Le Cong Nam <namlem4u@gmail.com>
2023-09-26 11:25:50 +0700

    feat/lc-3608

--- src/features/car/dto/car.dto.ts
+++ src/features/car/dto/car.dto.ts
@@ -41,6 +41,7 @@ export class IntegrationCar {
   rentalType?: string;
   fuelConsumption?: number;
   carStore?: number;
+  hiddenRentalPrice?: boolean;
   constructor(data: any = {}) {
     if (data) {
       this.carId = data.id;
@@ -83,6 +84,7 @@ export class IntegrationCar {
       this.rentalPrice = data.rentalPrice;
       this.fuelConsumption = data.fuelConsumption;
       this.carStore = data.carStore;
+      this.hiddenRentalPrice = data.hiddenRentalPrice;
     }
   }
 }
