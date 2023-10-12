5a5f4074af2d5b9d95c930bee2c97b57ceaf3eb1
Titus Pham <phamtien1709@gmail.com>
2023-10-12 13:18:31 +0700

    LC-1665/feat: Resolve comments

--- src/features/loyalty/campaigns/types/campaign.type.ts
+++ src/features/loyalty/campaigns/types/campaign.type.ts
@@ -14,15 +14,15 @@ export const CAMPAIGN_CONSTANT = {
     BOTH: 'both',
   },
   NAME: {
-    OPEN_APP_ONE_TIME_A_DAY: 'open_app_one_time_a_day',
-    COMPLETE_A_BOOKING: 'complete_a_booking',
-    COMPLETE_A_PURCHASE: 'complete_a_purchase',
+    // OPEN_APP_ONE_TIME_A_DAY: 'open_app_one_time_a_day',
+    // COMPLETE_A_BOOKING: 'complete_a_booking',
+    // COMPLETE_A_PURCHASE: 'complete_a_purchase',
     REFER_A_USER_REGISTRATION: 'refer_a_user_registration',
     REFER_A_COMPLETED_BOOKING: 'refer_a_completed_booking',
     REFER_A_COMPLETED_PURCHASE: 'refer_a_completed_purchase',
-    RATING_AND_REVIEW_APP: 'rating_and_review_app',
-    RATING_AND_REVIEW_A_BOOKING: 'rating_and_review_a_booking',
-    RATING_AND_REVIEW_A_PURCHASE: 'rating_and_review_a_purchase',
+    // RATING_AND_REVIEW_APP: 'rating_and_review_app',
+    // RATING_AND_REVIEW_A_BOOKING: 'rating_and_review_a_booking',
+    // RATING_AND_REVIEW_A_PURCHASE: 'rating_and_review_a_purchase',
   },
 };
 
