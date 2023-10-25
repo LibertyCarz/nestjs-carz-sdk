a83696e2d0ce541987d4956af4b8343b6270a8d0
Quan Luong <lhongquan.1998@gmail.com>
2023-10-25 10:32:00 +0700

    function sdk for use voucher code

--- src/features/loyalty/vouchers/dto/voucher.customer.dto.ts
+++ src/features/loyalty/vouchers/dto/voucher.customer.dto.ts
@@ -2,3 +2,9 @@ export type UserRedeemVoucherDTO = {
   userId: number;
   userType: number;
 };
+
+export type UseVoucherCodeDTO = {
+  userId: number;
+  userType: number;
+  codeId: string;
+};
--- src/features/loyalty/vouchers/services/voucher.customer.internal.service.ts
+++ src/features/loyalty/vouchers/services/voucher.customer.internal.service.ts
@@ -1,19 +1,18 @@
 import { HttpService } from '@nestjs/axios';
-import { Inject, Injectable } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { Injectable } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
-import { SERVICES } from '../../../../constants';
 import { BaseInternalRequest } from '../../../../types';
-import { ListVoucherDTO, UserRedeemVoucherDTO } from '../dto';
+import {
+  ListVoucherDTO,
+  UseVoucherCodeDTO,
+  UserRedeemVoucherDTO,
+} from '../dto';
 import { VoucherCode } from '../types';
 @Injectable()
 export class LoyaltyVoucherCustomerInternalService {
   private _endpoint =
     process.env.LOYALTY_SERVICE_ENDPOINT + 'customer/vouchers';
-  constructor(
-    @Inject(SERVICES.CARZ_LOYALTIES) private _carzLoyalty: ClientProxy,
-    private _httpService: HttpService,
-  ) {}
+  constructor(private _httpService: HttpService) {}
 
   public async getList(
     request: BaseInternalRequest<ListVoucherDTO>,
@@ -44,6 +43,20 @@ export class LoyaltyVoucherCustomerInternalService {
     const response = await lastValueFrom(
       this._httpService.post<BaseResponse<VoucherCode>>(
         `${this._endpoint}/redeem/${id}`,
+        payload,
+        request.buildRequestConfig(),
+      ),
+    );
+    return response.data.data;
+  }
+
+  public async useVoucherCode(
+    payload: UseVoucherCodeDTO,
+    request: BaseInternalRequest,
+  ) {
+    const response = await lastValueFrom(
+      this._httpService.post<BaseResponse<VoucherCode>>(
+        `${this._endpoint}/codes/use`,
         payload,
         request.buildRequestConfig(),
       ),
