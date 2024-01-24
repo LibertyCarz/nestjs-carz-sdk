a30814c888d578da5f9f46c2c5fef1d5f4e90e11
datnt2996 <datnt2996@gmail.com>
2024-01-24 18:06:15 +0700

    update base service

--- src/shared/base.service.ts
+++ src/shared/base.service.ts
@@ -1,11 +1,14 @@
-import { HttpException } from '@nestjs/common';
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { isAxiosError } from 'axios';
 export class BaseService {
   protected throwError(error: any) {
     if (isAxiosError(error)) {
-      throw new HttpException(error.response.data, error.response.status);
+      throw new HttpException(
+        error.response?.data,
+        error.response.status || HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     } else {
-      throw new HttpException(error.message, 500);
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
