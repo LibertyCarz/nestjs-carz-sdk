2ccc926a9e497595806b6a2b046e9c6540d907c9
datnt2996 <datnt2996@gmail.com>
2024-02-23 14:24:30 +0700

    add try catch

--- src/features/car-evaluation/car-evaluation.internal.service.ts
+++ src/features/car-evaluation/car-evaluation.internal.service.ts
@@ -1,23 +1,29 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
+import { BaseService } from '../../shared/base.service';
 import { CarEvaluation, CarEvaluationDto } from './car-evaluation.type';
 
 @Injectable()
-export class CarEvaluationInternalService {
+export class CarEvaluationInternalService extends BaseService {
   private _endpoint: string;
   constructor(private _httpService: HttpService) {
+    super();
     this._endpoint = process.env.CRAWLER_SERVICE_ENDPOINT + 'car-evaluations';
   }
 
   async getCarEvaluation(
     params: CarEvaluationDto,
   ): Promise<BaseResponse<CarEvaluation[]>> {
-    const response = await lastValueFrom(
-      this._httpService.get<BaseResponse<CarEvaluation[]>>(this._endpoint, {
-        params,
-      }),
-    );
-    return response.data;
+    try {
+      const response = await lastValueFrom(
+        this._httpService.get<BaseResponse<CarEvaluation[]>>(this._endpoint, {
+          params,
+        }),
+      );
+      return response.data;
+    } catch (error) {
+      this.throwError(error);
+    }
   }
 }
